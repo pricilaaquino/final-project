6da2e296d6fb0f420b1a667de783e2d6adcfe48e
pricilaaquino <jumpingshrimp27@yahoo.com>
2017-12-11 12:18:07 +0800

    Fix stuff

--- config.php
+++ config.php
@@ -3,4 +3,5 @@
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'juleski');
    define('DB_DATABASE', 'pri_lis');
+   // Connect to mysql using the credentials above
    $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
